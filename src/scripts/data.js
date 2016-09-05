c86a41334ec8cf0f349a22ab85d66ca3c6691e61
xuke <ke.x@maddog.tech>
2016-09-05 15:07:51 +0800

    wenzi

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -10,11 +10,23 @@ var pageData = {
                 "min-width": "80%",
                 "-ms-interpolation-mode": "bicubic"
             },
-            "middle":{
+            "right":{
+              "characte":"不要怂，就是干",
+              "characteEnglish":"Do not afraid,    Let's do it",
+              "locate":{
+                "font-size":"35pt",
+                "font-style":"italic",
+                "width": "600px",
+                "top":"45%",
+                "right":"30%",
+                'color':'#D2E9FF'
+                        }
+              }
+          /*  "middle":{
                 "isVideo":'video',
                 "picVideo": 'http://maddog-static.oss-cn-qingdao.aliyuncs.com/videoImg.jpeg',
                 "srcVideo":"../video/test1.mp4"
-            }
+            }*/
         },{
             "styleObject":{
                 height:HEIGHT+'px',
@@ -42,12 +54,16 @@ var pageData = {
               "characte":"我们提供嵌入式软硬件设计，快速原型设计，工业控制系统应用设计等服务，在每一步设计中都带着新的想法去超越。",
               "characteEnglish":"We offer rapid proto-typing and proto-type testing for all applications and Software development for embedded industrial control systems and applications With long experience in designing we  assist our customers in product design right from the start, bringing fresh ideas to every step in the design process and beyond, all the way towards manufacturing.",
               "middle":{
+                  "text-align":"justify",
+                  "font-size":"20pt",
+                  "font-size":"italic",
                   "position":"absolute",
                   "width":"500px",
-                  "text-align":'left',
-                  "right":"20px",
-                  "top":"28%",
-                  'color':'#ffffff'
+                /*  "margin-left":"left",*/
+                /*  "right":"25px",*/
+                  "left" :"60%",
+                  "bottom"  :"65%",
+                  'color':"#ffffff"
               }
             }
         },{
@@ -64,7 +80,8 @@ var pageData = {
               "locate":{
                 "width": "500px",
                 "top":"28%",
-                "left":"62%"
+                "left":"62%",
+                'color':'#000000'
               }
             }
         },{
@@ -76,11 +93,11 @@ var pageData = {
                 "-ms-interpolation-mode": "bicubic"
             },
             "right": {
-              "characteEnglish":"RF design is one of our strengths when it comes to making durable and reliable test environment for laboratories and manufacturing centers. We assist customers following their requirements while developing ways to achieving their goals in the design.",
+              "characteEnglish":"  RF design is one of our strengths when it comes to making durable and reliable test environment for laboratories and manufacturing centers. We assist customers following their requirements while developing ways to achieving their goals in the design.",
               "characte":"RF设计是我们的优势之一，我们在开发设计过程中按照他们的需求来完成产品的设计。",
               "locate":{
                 "width": "50%",
-                "top":"60%",
+                "top":"55%",
                 "right":"15%"
               }
             }
@@ -98,7 +115,7 @@ var pageData = {
               "characteEnglish":"HVAC system can control the air temperature and humidity in your place and maintain high air quality. It is convenient to you and maintenance workers to quickly understand the status of HVAC and timely correction and warning.",
               "locate":{
                 "width": "400px",
-                "top":"50%",
+                "top":"45%",
                 "right":"40%"
               }
             }
@@ -153,11 +170,11 @@ var pageData = {
               "characte":"地址:中国成都市郫县天润路777号学府海棠1-2-1802 电话:028-87841139",
               "characteEnglish":"No.1-2-1802, Xuefuhaitang, Tianrun RD No.777, Pixian, Chengdu   Tel:028-87841139",
               "locate":{
-                "font-style":"oblique",
+                "font-style":"Normal",
                 "width": "1000px",
                 "bottom":"100px",
                 "right":"20%",
-                "bottom":"35px"
+                'color':'#ffffff'
               }
             }
         }],
@@ -174,7 +191,7 @@ var pageData = {
                 "characte":"MadDog是一家致力于嵌入式开发团队的工作室。过硬的实力，耐心地倾听，实时通信和良好的管理是我们比其他同业更好的原因。",
                 "characteEnglish":"Mad Dog is a studio dedicated to embedded development team. Excellent strength, patiently listening, real-time communication and good management are the reasons that we are better than others in the industry.",
                 "locate":{
-                    "width":"500px",
+                    "width":"700px",
                     "height":"200px",
                     "top":"50%",
                     "left":"50%",
